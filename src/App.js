18e40bcb6bfc3eb624fc9e4e12b0a4f63336a486
dattranDSS <dat.tran@dssolution.com>
2023-07-01 20:50:53 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
-import ReactGa from "react-ga4";
+import ReactGA from "react-ga4";
 
 import { publicRoutes } from "~/routes";
 import DefaultLayout from "~/layouts";
 
-ReactGa.initialize('G-BT4Y98K8N0');
+ReactGA.initialize("G-BT4Y98K8N0");
 
 function App() {
   useEffect(() => {
-    ReactGa.send({
+    ReactGA.send({
       hitType: "pageview",
       page: document.location.pathname,
       title: "Home page"
     });
-    // ReactGa.pageview(document.location.pathname);
+    // ReactGA.pageview(document.location.pathname);
 
     console.log("google-analytics");
   }, []);
